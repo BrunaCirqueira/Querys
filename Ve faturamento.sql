5ec5ee71080497e03869577e7597f06d0ce86cab
BrunaCirqueira <brunajheci@hotmail.com>
2018-08-03 10:32:09 -0300

    Ve faturamento
    ADD base de homologação

--- Ve faturamento.sql
+++ Ve faturamento.sql
@@ -1,1 +1,11 @@
-select count(distinct U_FLX_IdFatSIGEM)  from oinv where CreateDate between '17-07-2018' and '17-07-2018' and UserSign=1 
+select sum (total) from (
+select count(distinct U_FLX_IdFatSIGEM) total from SBO_GERENCIADORA_PRODUCAO..OINV  where CreateDate between '13-07-2018' and '02-08-2018' and UserSign=1 
+union all
+select count(distinct U_FLX_IdFatSIGEM) total from SBO_GERENCIADORA_HOMOLOGACAO..OINV  where CreateDate between '13-07-2018' and '02-08-2018' and UserSign=1 
+) Faturas
+
+
+--205283 16:39
+
+
+ 
